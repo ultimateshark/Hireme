cb84644b8a88392deec6c8e8bff852c6bcbd230d
ultimateshark <ultimateshark@localhost.localdomain>
2020-01-11 20:43:31 +0530

    apply

--- init.py
+++ init.py
@@ -558,7 +558,7 @@ def Applied_To():
 		return redirect(url_for("Home"))
 
 @app.route("/apply-to-page/<int:job_id>")
-@login_required
+@condidate_login_required
 def Apply_To_Job_page(job_id):
 	try:
 		try:
